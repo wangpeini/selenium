c25f83c1ec4acf48a4ef26f95bf2d91dffd2650a
wangpeini <752937680@qq.com>
2018-12-28 10:30:04 +0800

    获取网页标题文本增加断言

--- 003.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-driver = webdriver.Chrome()
-driver.get("https://www.baidu.com")
-driver.maximize_window()
-driver.find_element_by_css_selector('span[class="soutu-btn"]').click()
-driver.find_element_by_css_selector('input[type="file"]').send_keys("C://Users//Public//Pictures//Sample Pictures//1.jpg")
---
+++ 006.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://www.baidu.com")
+print(driver.title)#获取网页标题
+assert "百度" in driver.title  #增加判断assert
+driver.find_element_by_id("kw").send_keys("流星花园")
+
+driver.find_element_by_id("su").click()
+l = driver.find_element_by_id("content_left").text#拿到文本值text
+print(l)
+assert "流星花园" in l
